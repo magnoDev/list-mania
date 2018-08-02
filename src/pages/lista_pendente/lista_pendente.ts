b254ba6d51c3aec3dc815300ee095f7d88cd088a
Magno Macedo <magno@example.com>
2018-08-02 09:24:25 -0300

    feat(Start): Começo de tudo

--- src/pages/lista_pendente/lista_pendente.ts
+++ src/pages/lista_pendente/lista_pendente.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-lista_pendente',
+  templateUrl: 'lista_pendente.html'
 })
-export class AboutPage {
+export class ListaPendentePage {
 
   constructor(public navCtrl: NavController) {
 
